dec07049933047cb741a00680dc4ceb9d2d6ab52
CHARLIECODERR <vedantchavanjolly@gmail.com>
2025-03-17 01:57:04 +0530

    Update Hellotest.java

--- Hellotest.java
+++ Hellotest.java
@@ -1,1 +1,14 @@
-
+public class HelloTest {
+  public static void main(String[] args){
+    if (Hello.isPositive(5)) {
+      System.out.println("Test Passed : 5 is  positive");
+    } else {
+       System.out.println("Test failed");
+    }
+    if (Hello.isPositive(-3)) {
+       System.out.println("Test Passed : -3 is  pnot ositive");
+    } else {
+       System.out.println("Test failed");
+    }
+  }
+}
